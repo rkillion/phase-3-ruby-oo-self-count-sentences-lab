f810df49f3b352de9ebb081cbb5e559824a8cd8a
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-31 17:18:09 -0400

    commit tree de9d24be393d60c2956869327e9c82498b8d5f69

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1]=="."
   end
 
   def question?
-
+    self[-1]=="?"
   end
 
   def exclamation?
-
+    self[-1]=="!"
   end
 
   def count_sentences
-
+    sentences = self.scan(/[^.?!]+[.?!]/)
+    sentences.length
   end
 end
+
+# binding.pry
